e59614391be40b9ac6a06b859b057c4bbaec231b
LISOVSMA <116366290+LISOVSMA@users.noreply.github.com>
2023-08-09 23:59:41 +0300

    add task

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,9 @@
 const emptyBody = require("./emptyBody");
 const validateBody = require("./validateBody");
+const isValidId = require("./isValidId");
 
 module.exports = {
   emptyBody,
   validateBody,
+  isValidId,
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,22 +1,30 @@
 const express = require("express");
 const ctrl = require("../../controllers/contacts");
-const schemas = require("../../schemas/contacts");
-const { emptyBody, validateBody } = require("../../middlewares");
+const { schemas } = require("../../models/contact");
+const { emptyBody, validateBody, isValidId } = require("../../middlewares");
 const router = express.Router();
 
 router.get("/", ctrl.listContacts);
 
-router.get("/:id", ctrl.getContactById);
+router.get("/:id", isValidId, ctrl.getContactById);
 
 router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
 
-router.delete("/:id", ctrl.removeContact);
+router.delete("/:id", isValidId, ctrl.removeContact);
 
 router.put(
   "/:id",
+  isValidId,
   emptyBody,
   validateBody(schemas.addSchema),
   ctrl.updateContact
+);
+
+router.patch(
+  "/:id/favorite",
+  isValidId,
+  validateBody(schemas.updateFavoreteSchema),
+  ctrl.updateStatusContact
 );
 
 module.exports = router;
